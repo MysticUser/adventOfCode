68f925bf86ed910585c3b11f2e36cb00fc2a0d0a
Eirik <ethiles@online.no>
2020-12-04 13:21:42 +0100

    Golang solution day 4

---
+++ golang/day04.go
@@ -0,0 +1,118 @@
+package golang
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func Day04(rawInput string) {
+	fmt.Print("This is day 04 code:\n\n")
+	var matchCountPartOne, matchCountPartTwo int
+
+	var inputRows = strings.Split(rawInput, "\n\n")
+
+	for _, row := range inputRows {
+		row = strings.Replace(row, "\n", " ", -1)
+
+		var validKeysP1, validKeysP2 int
+		var missingCID = true
+
+		for _, passportData := range strings.Split(row, " ") {
+			passportKeys := strings.Split(passportData, ":")
+
+			if len(passportKeys) != 2 {
+				continue
+			}
+
+			key := passportKeys[0]
+			value := passportKeys[1]
+
+			if key == "cid" {
+				missingCID = false
+			}
+
+			if key != "" && value != "" {
+				validKeysP1++
+
+				switch key {
+				case "byr":
+					byr, err := strconv.Atoi(value)
+					if byr < 1920 || byr > 2002 || err != nil {
+						continue
+					}
+
+				case "iyr":
+					iyr, err := strconv.Atoi(value)
+					if iyr < 2010 || iyr > 2020 || err != nil {
+						continue
+					}
+
+				case "eyr":
+					eyr, err := strconv.Atoi(value)
+					if eyr < 2020 || eyr > 2030 || err != nil {
+						continue
+					}
+
+				case "hgt":
+					if len(value) < 3 {
+						continue
+					}
+
+					unit := value[len(value)-2:]
+					hgt, err := strconv.Atoi(value[:len(value)-2])
+					if (unit == "cm" && (hgt < 150 || hgt > 193)) || (unit == "in" && (hgt < 59 || hgt > 76) || err != nil) {
+						continue
+					}
+
+				case "hcl":
+					match, _ := regexp.Match("^#[0-9a-f]{6}$", []byte(value))
+					if !match {
+						continue
+					}
+
+				case "ecl":
+					validColors := [7]string{"amb", "blu", "brn", "gry", "grn", "hzl", "oth"}
+
+					if !contains(validColors, value) {
+						continue
+					}
+
+				case "pid":
+					if len(value) != 9 {
+						continue
+					}
+
+				case "cid":
+
+				default:
+					continue
+				}
+
+				validKeysP2++
+			}
+		}
+
+		if validKeysP1 == 8 || (validKeysP1 == 7 && missingCID) {
+			matchCountPartOne++
+		}
+
+		if validKeysP2 == 8 || (validKeysP2 == 7 && missingCID) {
+			matchCountPartTwo++
+		}
+	}
+
+	fmt.Println("Valid passports part one: ", matchCountPartOne)
+	fmt.Println("Valid passports part two: ", matchCountPartTwo)
+	// took 134 min
+}
+
+func contains(arr [7]string, str string) bool {
+	for _, a := range arr {
+		if a == str {
+			return true
+		}
+	}
+	return false
+}
--- main.go
+++ main.go
@@ -10,20 +10,28 @@ import (
 func main() {
 	fmt.Println()
 
-	/* NAVIGATOR */
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter a day: ")
-	day, _ := reader.ReadString('\n')
+	var day, input string
+	var useNavigator bool
 
-	fmt.Println("Paste input (press tab and enter to continue):")
-	input, _ := reader.ReadString('\t')
-	fmt.Println()
-	/* * /
+	useNavigator = true
 
-	/* Development usage * /
-	day := "3\n"
-	input := "..##.......\n#...#...#..\n.#....#..#.\n..#.#...#.#\n.#...##..#.\n..#.##.....\n.#.#.#....#\n.#........#\n#.##...#...\n#...##....#\n.#..#...#.#"
-	/* */
+	if useNavigator {
+		/* NAVIGATOR */
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Enter a day: ")
+		day, _ = reader.ReadString('\n')
+
+		fmt.Println("Paste input (press tab and enter to continue):")
+		input, _ = reader.ReadString('\t')
+		fmt.Println()
+		/* */
+	} else {
+		/* Development usage */
+		day = "4\n"
+		//input = "eyr:1972 cid:100\nhcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926\n\niyr:2019\nhcl:#602927 eyr:1967 hgt:170cm\necl:grn pid:012533040 byr:1946\n\nhcl:dab227 iyr:2012\necl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277\n\nhgt:59cm ecl:zzz\neyr:2038 hcl:74454a iyr:2023\npid:3556412378 byr:2007"
+		input = "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980\nhcl:#623a2f\n\neyr:2029 ecl:blu cid:129 byr:1989\niyr:2014 pid:896056539 hcl:#a97842 hgt:165cm\n\nhcl:#888785\nhgt:164cm byr:2001 iyr:2015 cid:88\npid:545766238 ecl:hzl\neyr:2022\n\niyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719"
+		/* */
+	}
 
 	switch day {
 	case "1\n":
@@ -32,6 +40,8 @@ func main() {
 		aoc.Day02(input)
 	case "3\n":
 		aoc.Day03(input)
+	case "4\n":
+		aoc.Day04(input)
 	default:
 		fmt.Println("Not found!")
 	}
